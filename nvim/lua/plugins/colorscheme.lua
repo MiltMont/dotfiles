53faf0f719aafe29ddd032b103ba116b152cdafc
MiltMont <miltgonmont@gmail.com>
2025-11-08 20:13:36 -0600

    Backup

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,34 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        terminal_colors = true,
+        styles = {
+          commments = "undercurl",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+        inverse = {
+          match_paren = true,
+          visual = true,
+          search = true,
+        },
+        darken = {
+          floats = false,
+        },
+      })
+      vim.cmd("colorscheme github_dark")
+    end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "github_dark_dimmed",
     },
   },
 }
